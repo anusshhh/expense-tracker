44606554d1a7bf51f88c47389ccd30f69dc19eac
Anush Padubidri <anush.hkp@gmail.com>
2024-06-25 11:52:05 +0530

    project set up

---
+++ src/main/java/com/budgetmaster/expensetracker/entity/UserEntity.java
@@ -0,0 +1,10 @@
+package com.budgetmaster.expensetracker.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class UserEntity {
+
+}
---
+++ src/main/java/com/budgetmaster/expensetracker/service/IAuthenticationService.java
@@ -0,0 +1,8 @@
+package com.budgetmaster.expensetracker.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IAuthenticationService {
+
+}
